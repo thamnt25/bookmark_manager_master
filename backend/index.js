398df0e228ccaaf0d7777ab52b3c08bfc5bd8b42
thamnt <thamnt258@gmail.com>
2026-04-26 22:10:59 -0600

    add ing cros and fetch all bookmarks from db

--- backend/index.js
+++ backend/index.js
@@ -1,8 +1,10 @@
 const express = require("express");
 const app = express();
+const cors = require('cors');
 const router = require("./router/api");
 const port = 3000;
 
+app.use(cors());
 app.use(express.json());
 app.use("/", router);
 
--- backend/router/bookmarkRouter.js
+++ backend/router/bookmarkRouter.js
@@ -1,8 +1,14 @@
 const express = require("express");
 const router = express.Router();
+const { getAllBookMarks } = require("../firebase/bookMark");
 
-router.get("/", (req, res, next) => {
-  res.send("Bookmark rounter home page");
+router.get("/", async (req, res, next) => {
+  try {
+    const bookMarks = await getAllBookMarks();
+    res.status(200).json(bookMarks);
+  } catch (e) {
+    console.error(e);
+  }
 });
 
 module.exports = router;
